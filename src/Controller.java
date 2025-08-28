dbe7fbd0d10ddfe30c6b859ff14bdec86b4aeb15
Aine <ainekilbane@gmail.com>
2025-08-28 11:09:57 +0100

    made a function in API that will return a hardcoded JSON to the frontend

--- src/Controller.java
+++ src/Controller.java
@@ -1,16 +1,14 @@
 import com.sun.net.httpserver.HttpServer;
-
+import java.io.*;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.InetSocketAddress;
 import java.net.URLDecoder;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.sql.ResultSet;
+import java.util.*;
 import java.util.regex.Pattern;
-
 import static java.util.stream.Collectors.*;
+
 
 public class Controller {
         public static void main(String[] args) throws Exception {
@@ -21,13 +19,16 @@ public class Controller {
 
                 if ("GET".equals(exchange.getRequestMethod())) {
                     Map<String, List<String>> params = splitQuery(exchange.getRequestURI().getRawQuery());
-                    String noNameText = "Anonymous";
-                    String name = params.getOrDefault("name", List.of(noNameText)).stream().findFirst().orElse(noNameText);
-                    String respText = String.format("Hello %s!", name); //forms body
+                    String query = params.get("query").getFirst();
+
                     exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
-                    exchange.getResponseHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-                    exchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
-                    exchange.sendResponseHeaders(200, respText.getBytes().length); //200 OK
+                    exchange.getResponseHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT");
+                    exchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type: application/json");
+
+                    //String respText = String.format("Hello %s!", query); //forms body
+                    String respText = ConvertRS();
+
+                    exchange.sendResponseHeaders(200, respText.getBytes().length); //200 OK measures output length
                     OutputStream output = exchange.getResponseBody();
                     output.write(respText.getBytes());
                     output.flush();
@@ -43,7 +44,7 @@ public class Controller {
         }
 
         public static Map<String, List<String>> splitQuery(String query) {
-            if (query == null || "".equals(query)) {
+            if (query == null || query.isEmpty()) {
                 return Collections.emptyMap();
             }
             return Pattern.compile("&").splitAsStream(query)
@@ -57,5 +58,24 @@ public class Controller {
             } catch (final UnsupportedEncodingException e) {
                 throw new RuntimeException("UTF-8 is a required encoding", e);
             }
+        }
+
+        private static String ConvertRS () {
+            //will receive a resultset, and loop through it assigning a key and value to a JSON shaped string for each row
+            //single line of hardcoded data present for testing and developing the dynamic table
+            String respText = "{";
+            int index = 0;
+            index++;
+            String film = "film";
+            String genre = "genre";
+            String leadStudio = "studio";
+            String audienceScore = "100";
+            String profitability = "profit";
+            String rottenTomatoes = "70";
+            String worldwideGross = "profit";
+            String year = "2030";
+            respText += String.format("\"%d\": \"%s, %s, %s, %s, %s, %s, %s, %s\"",index, film, genre, leadStudio, audienceScore, profitability, rottenTomatoes, worldwideGross, year);
+            respText += "}";
+            return respText;
         }
 }
